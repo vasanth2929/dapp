a7f5c408a1df0978f52939edb280ec2f314b1c42
vasanth <vasanth@kodepi.com>
2022-02-13 09:42:53 +0530

    init base code and moralis config setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      serverUrl={process.env.REACT_PUBLIC_SERVER_URL}
+      appId={process.env.REACT_PUBLIC_APP_ID}
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
